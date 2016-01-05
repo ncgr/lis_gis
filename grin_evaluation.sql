3df04f3308970927ba18300bc500945d11a223a4
Alex Rice <agr@ncgr.org>
2016-01-05 10:35:39 -0700

    Add notes about how to create trigger and function for accenumb field.

--- grin_evaluation.sql
+++ grin_evaluation.sql
@@ -11,3 +11,19 @@ ON lis_germplasm.legumes_grin_evaluation_data (lower(taxon));
 
 UPDATE lis_germplasm.legumes_grin_evaluation_data
 SET accenumb = accession_prefix || ' ' || accession_number;
+
+
+CREATE FUNCTION grin_evaluation_data_concat_accenumb() RETURNS trigger AS
+  $concat_accenumb$
+  BEGIN
+      NEW.accenumb = NEW.accession_prefix || ' ' || NEW.accession_number;
+      RETURN NEW;
+  END
+  $concat_accenumb$
+LANGUAGE plpgsql
+;
+
+CREATE TRIGGER accenumb_trigger
+BEFORE INSERT ON lis_germplasm.legumes_grin_evaluation_data
+FOR EACH ROW EXECUTE PROCEDURE grin_evaluation_data_concat_accenumb()
+;
