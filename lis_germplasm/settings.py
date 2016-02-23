ae619762a0ce525a942412977ab2fd904024c5a1
Alex Rice <agr@ncgr.org>
2016-02-23 11:47:20 -0600

    Read django secret key from file instead of hardcode in settings.py.

--- lis_germplasm/settings.py
+++ lis_germplasm/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '---change me---'
+SECRET_KEY = open(os.path.join(os.path.dirname(__file__),
+                               'django_secret_key.txt')).read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
