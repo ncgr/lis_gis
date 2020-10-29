ff811e476a4bdcfc12d02198aa56d33e28b1c55d
Nathan Weeks <1800812+nathanweeks@users.noreply.github.com>
2020-10-29 10:08:19 -0500

    Upgrade from Django 2.2.x -> 3.1.x

--- lis_germplasm/settings.py
+++ lis_germplasm/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from pathlib import Path
 import os
 from django.core.management.utils import get_random_secret_key
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
--- lis_germplasm/urls.py
+++ lis_germplasm/urls.py
@@ -1,32 +1,31 @@
 """lis_germplasm URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-# from django.contrib import admin
+from django.urls import path
 from grin_app import views as grin_views
 
 urlpatterns = [
 
-    url(r'^$', grin_views.index),
-    url(r'^search$', grin_views.search),
-    url(r'^countries$', grin_views.countries),
-    url(r'^accession_detail$', grin_views.accession_detail),
-    url(r'^evaluation_descr_names$', grin_views.evaluation_descr_names),
-    url(r'^evaluation_detail$', grin_views.evaluation_detail),
-    url(r'^evaluation_search$', grin_views.evaluation_search),
-    url(r'^evaluation_metadata$', grin_views.evaluation_metadata),
+    path('', grin_views.index),
+    path('search', grin_views.search),
+    path('countries', grin_views.countries),
+    path('accession_detail', grin_views.accession_detail),
+    path('evaluation_descr_names', grin_views.evaluation_descr_names),
+    path('evaluation_detail', grin_views.evaluation_detail),
+    path('evaluation_search', grin_views.evaluation_search),
+    path('evaluation_metadata', grin_views.evaluation_metadata),
 
     # disabling admin site until it's actually required/used --agr
     # url(r'^admin/', include(admin.site.urls)),
