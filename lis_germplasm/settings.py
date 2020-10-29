bcf399b6a523ec1bb5278755034d2dbfd48917cd
Nathan Weeks <1800812+nathanweeks@users.noreply.github.com>
2020-10-29 05:48:17 -0500

    Hard-code Django STATIC_URL

--- lis_germplasm/settings.py
+++ lis_germplasm/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = os.environ['STATIC_URL']
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
